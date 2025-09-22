1638aa2aff5aa3318b6e2b47a9c9b2a3b69403b2
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-09-22 22:43:51 +0200

    chore(ci): run with -race too
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- fuzz.sh
+++ fuzz.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
-go test -fuzz=FuzzCreateHashUnique -fuzztime=120s
-go test -fuzz=FuzzComparePasswordAndHash -fuzztime=120s
+go test -fuzz=FuzzGetClientIP -fuzztime=120s
+go test -fuzz=FuzzAllow -fuzztime=240s
+
+go test -fuzz=FuzzGetClientIP -race -fuzztime=20s
+go test -fuzz=FuzzAllow -race -fuzztime=20s
